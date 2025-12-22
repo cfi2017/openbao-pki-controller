403f78e4a9aa9388f7b3e883cb6364676c0f215a
Carlo Field <carlo@swiss.dev>
2025-12-22 08:45:28 +0100

    chore: add some linting and devenv setup
    Signed-off-by: Carlo Field <carlo@swiss.dev>

--- devenv.nix
+++ devenv.nix
@@ -5,48 +5,40 @@
   inputs,
   ...
 }: {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  packages = [pkgs.git pkgs.kubernetes-helm pkgs.kubectl pkgs.kind];
 
-  # https://devenv.sh/packages/
-  packages = [pkgs.git];
-
-  # https://devenv.sh/languages/
   languages.rust = {
     enable = true;
   };
 
-  # https://devenv.sh/processes/
-  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+  tasks."build:release" = {
+    exec = ''
+      docker build -t openbao-pki-controller -f Dockerfile .
+    '';
+  };
 
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
-  # https://devenv.sh/basics/
-  enterShell = ''
-    hello         # Run scripts directly
-    git --version # Use packages
-  '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
   enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
+    cargo test
   '';
 
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
+  git-hooks.hooks = {
+    # github actions
+    actionlint.enable = true;
+    action-validator.enable = true;
 
-  # See full reference at https://devenv.sh/reference/options/
+    # might as well lint nix
+    nil.enable = true;
+    nixfmt-rfc-style.enable = true;
+
+    # rust
+    clippy.enable = true;
+    clippy.settings.allFeatures = true;
+    clippy.settings.denyWarnings = true;
+    cargo-check.enable = true;
+    rustfmt.enable = true;
+
+    # helm
+    # this one just runs ct lint --all --skip-dependencies
+    # chart-testing.enable = true;
+  };
 }
